77a99696d2dc50b8d1a90bcd0f78f63bfccc0207
thirumurugan7 <thiru82003@gmail.com>
2023-07-07 14:49:50 +0530

    navbar done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <section className="bg-black mt-[00px]">
+      <div className="hero1 h-screen">
+        <Navbar />
+        <h1 className="text-black"> here? </h1>
+      </div>
+    </section>
+  );
+}
+
+export default App;
